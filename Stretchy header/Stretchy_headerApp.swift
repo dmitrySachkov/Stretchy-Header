d96a27fde62859410f7dddf7925bb096e46eb311
domont <d.a.sachkov@gmail.com>
2025-06-16 16:27:44 +0300

    Initial Commit

---
+++ Stretchy header/Stretchy_headerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Stretchy_headerApp.swift
+//  Stretchy header
+//
+//  Created by Dmitry Sachkov on 16.06.2025.
+//
+
+import SwiftUI
+
+@main
+struct Stretchy_headerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
